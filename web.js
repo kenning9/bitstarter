ed95c4ca49c749b6de18638e44e111efed6f542d
kenning9 <fengjiahai@yahoo.com.sg>
2013-07-10 19:05:01 +0800

    part 2 hw 3

--- web.js
+++ web.js
@@ -1,9 +1,12 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
-
+var buffer = fs.readFileSync("index.html");
+var outp = buffer.toString();
+response.send("yay potato");
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(outp);
 });
 
 var port = process.env.PORT || 5000;
